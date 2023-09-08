3aee6c2e73f65389d90373fe436ae1f2a429590e
Casido1 <ugochukwu.anunihu@gmail.com>
2023-09-08 02:24:38 +0100

    Added user entity and DbContext

---
+++ DatingAppAPI/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingAppAPI.Entities
+{
+    public class AppUser
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string UserName { get; set; }
+    }
+}
