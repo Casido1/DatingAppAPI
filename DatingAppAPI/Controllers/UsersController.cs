a81e99cbc58a1d72e43ffc94c70933080d951b99
Casido1 <ugochukwu.anunihu@gmail.com>
2023-09-09 23:40:47 +0100

    Added user controller

---
+++ DatingAppAPI/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using DatingAppAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DatingAppContext _context;
+
+        public UsersController(DatingAppContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetUserById(string Id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == Id);
+
+            return Ok(user);
+        }
+    }
+}
--- DatingAppAPI/Program.cs
+++ DatingAppAPI/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<DatingAppContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
