ea6e5a1c6b2a308adab20b3eb98536e61b0a8cfc
Casido1 <ugochukwu.anunihu@gmail.com>
2023-09-27 21:51:20 +0100

    Added whitespaces to the userDTO

--- DatingAppAPI/DTOs/LoginDTO.cs
+++ DatingAppAPI/DTOs/LoginDTO.cs
@@ -3,6 +3,7 @@
     public class LoginDTO
     {
         public string UserName { get; set; }
+
         public string Password { get; set; }
     }
 }
--- DatingAppAPI/DTOs/UserDTO.cs
+++ DatingAppAPI/DTOs/UserDTO.cs
@@ -3,6 +3,7 @@
     public class UserDTO
     {
         public string UserName { get; set; }
+
         public string Token { get; set; }
     }
 }
